ed60d668464671c57e131db5c6195a1d5a396c56
thuya, linn khant <lkty07@gmail.com>
2023-03-15 15:11:44 -0700

    Added a correct market to location on the map

--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -3,6 +3,11 @@ const map = new mapboxgl.Map({
 container: 'map', // container ID
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
+center: campground.geometry.coordinates, // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
+
+// Create a new marker.
+const marker = new mapboxgl.Marker()
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
