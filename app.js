fe99a3101666408f0479abdd2a2af2285ee1a327
thuya, linn khant <lkty07@gmail.com>
2023-03-20 14:29:23 -0700

    added security measures - sanitizeHTML with Joi, helmet

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 if(process.env.NODE_ENV != "production"){
     require("dotenv").config()
 }
-    
+
 //require stuff
 const express = require('express');
 const app = express();
@@ -22,11 +22,13 @@ const reviewRoutes = require("./routes/reviews")
 //for session
 const session = require('express-session')
 const sessionConfig = {
+    name: 'yourSession',
     secret: "siuuuucret", 
     httpOnly: true,
     resave: false, 
     saveUninitialized: true,
     cookie: {
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -72,9 +74,70 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+//for mongo sanitize
+const mongoSanitize = require('express-mongo-sanitize');
+app.use(mongoSanitize());
+
+//for helmet
+const helmet = require('helmet');
+
+
+//tweaking contentsecuritypolicy to allow certain resources
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+//This is the array that needs added to
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://stackpath.bootstrapcdn.com/"
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+    "https://api.mapbox.com/mapbox-gl-js/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dznf6c94r/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+); //my cloudinary account - dznf6c94r
+//https://res.cloudinary.com/dznf6c94r/image/upload/
+
+
 //connect to mongoDB  using mongoose
 const mongoose = require("mongoose");
-const review = require('./models/review');
+
+const dbUrl = process.env.DB_URL
+//'mongodb://127.0.0.1:27017/yelp-camp'
+//dbUrl
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
 const db = mongoose.connection; //fancier, cleaner and to close the connection in the future
--- schemas.js
+++ schemas.js
@@ -1,5 +1,50 @@
 //require joi first
-const Joi = require('joi')
+const BaseJoi = require('joi')
+const sanitizeHTML = require('sanitize-html')
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#lable}} must not include HTML!'
+    },
+
+    rules:{
+        escapeHTML:{
+            validate(value, helpers){
+                const clean = sanitizeHTML(value, {
+                    allowedTags: [],
+                    allowedAtrributes: {},
+                });
+
+                if(clean !== value) return helpers.error('string.escapeHTML', {value})
+                    return clean;
+            }
+        }
+    }
+})
+const Joi = BaseJoi.extend(extension)
+
+module.exports.campgroundSchema = Joi.object({
+    //campground is a key inside the object, and its properties are also objects
+    campground: Joi.object({
+        //its keys
+        title: Joi.string().required().escapeHTML(),
+        price: Joi.number().required().min(0),
+        location: Joi.string().required().escapeHTML(),
+        //image: Joi.string().required(),
+        description: Joi.string().escapeHTML()
+
+    }).required(), //campground as a key is also required
+
+    deleteImages: Joi.array()
+})
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        text: Joi.string().required().escapeHTML(),
+        rating: Joi.number().required().min(1).max(5)
+    }).required()
+})
 
 /*
 //joi schema validator //Joi.{data type}
@@ -16,25 +61,3 @@ const campgroundSchema = Joi.object({
     }).required() //campground as a key is also required
 })
 */
-
-module.exports.campgroundSchema = Joi.object({
-    //campground is a key inside the object, and its properties are also objects
-    campground: Joi.object({
-        //its keys
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        location: Joi.string().required(),
-        //image: Joi.string().required(),
-        description: Joi.string()
-
-    }).required(), //campground as a key is also required
-
-    deleteImages: Joi.array()
-})
-
-module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        text: Joi.string().required(),
-        rating: Joi.number().required().min(1).max(5)
-    }).required()
-})
