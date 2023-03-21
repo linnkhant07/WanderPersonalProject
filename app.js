e2c8517217036719816407784e90ba1483f03240
thuya, linn khant <lkty07@gmail.com>
2023-03-21 12:27:31 -0700

    fixed dbUrl to mongo Atlas

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const landmarkRoutes = require('./routes/landmarks')
 const reviewRoutes = require("./routes/reviews")
 
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
 //'mongodb://127.0.0.1:27017/yelp-camp'
 //process.env.DB_URL
 
