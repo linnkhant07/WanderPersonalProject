2d0d74321e4350d79cca20960b956d880039333b
thuya, linn khant <lkty07@gmail.com>
2023-03-21 00:29:14 -0700

    Installed mongoDB module

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //.env
 if(process.env.NODE_ENV != "production"){
-    require("dotenv").config()
+    //require("dotenv").config()
 }
+
+require("dotenv").config()
 
 //require stuff
 const express = require('express');
