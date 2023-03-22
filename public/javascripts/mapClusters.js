b38909a964d12652cd3ae335a1d80a0d7ceaf223
thuya, linn khant <lkty07@gmail.com>
2023-03-22 12:18:55 -0700

    change mapbox center to myanmar

--- public/javascripts/mapClusters.js
+++ public/javascripts/mapClusters.js
@@ -3,7 +3,7 @@
     container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-103.5917, 40.6699],
+    center: [96.12588, 16.83591],
     zoom: 3
     });
 
